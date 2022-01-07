b638723265021d6ced1607168eebea2e86bcd2cd
Admirl44 <36560341+admiral44@users.noreply.github.com>
2022-01-08 02:12:45 +0530

    How to set defoult Page or Page not found Page.

--- exproject-2/app.js
+++ exproject-2/app.js
@@ -3,8 +3,14 @@ import express from 'express';
 const app = express();
 const port = process.env.port || '3000';
 
+// this how we can set get methode and route in Express.
 app.get('/', (req, res) => {
     res.send("Hello shubham walcome to Express Rounting project-2");
+});
+
+//How to set defoult Page or Page not found Page.
+app.all('*', (req, res) => {
+    res.send("Page Not found ..!!!");
 });
 
 app.listen( port, () => {
