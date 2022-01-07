d3fff39c33eb04044c6c82c80c5c701b3b76f63b
Admirl44 <36560341+admiral44@users.noreply.github.com>
2022-01-07 21:53:21 +0530

    master

--- exproject-1/app.js
+++ exproject-1/app.js
@@ -1,1 +1,9 @@
 // First Express module.
+import express, { application } from 'express';
+const app = express();
+const port = process.env.port || '3000';
+
+
+app.listen(port, ()=> {
+    console.log(`Server listeing at http://localhost:${port}`)
+});
