58a66897b9e9490221f9b077a508f3990eeef0d5
Admirl44 <36560341+admiral44@users.noreply.github.com>
2022-01-08 01:20:10 +0530

    add app.get() in app.js

--- exproject-1/app.js
+++ exproject-1/app.js
@@ -3,6 +3,9 @@ import express, { application } from 'express';
 const app = express();
 const port = process.env.port || '3000';
 
+app.get('/',(req, res) => {
+    res.send('Hello shubham .!')
+});
 
 app.listen(port, ()=> {
     console.log(`Server listeing at http://localhost:${port}`)
